201035e3479cd15a33d77e53326dad6d21d7a522
vinayakgarole <vinayakgarole10@gamil.com>
2023-05-03 14:12:18 +0530

    Insert the data in table

---
+++ InsertData.php
@@ -0,0 +1,31 @@
+<?php
+// Connecting to the Database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "dbvinayak";
+
+// Create a connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Die if connection was not successful
+if (!$conn) {
+    die("Sorry we failed to connect: " . mysqli_connect_error());
+} else {
+    echo "Connection was successful <br>";
+}
+
+// Variables to be inserted into the table
+$name = "Vikrant";
+$destination = "Russia";
+
+// SQL query to be executed
+$sql = "INSERT INTO `phptrip` (`Name`, `Dest`) VALUES ('$name', '$destination')";
+$result = mysqli_query($conn, $sql);
+// Add a new trip to the Trip table in the database
+if ($result) {
+    echo "The record has been inserted successfully!";
+} else {
+    echo "Therecord was not inserted successfully because of this eroor ---> " . mysqli_error($conn);
+}
+?>
