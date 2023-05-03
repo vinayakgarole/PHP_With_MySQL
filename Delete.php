cebbc6dfbe26ac3a22ecbfb2ddbb4ad3e934c87d
vinayakgarole <vinayakgarole10@gamil.com>
2023-05-03 18:18:06 +0530

    Delete from the data in database

---
+++ Delete.php
@@ -0,0 +1,29 @@
+<?php
+// Connecting to the Database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "dbvinayak";
+
+// Create a connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Die if connection was not successful
+if (!$conn) {
+    die("Sorry we failed to connect: " . mysqli_connect_error());
+} else {
+    echo "Connection was successful <br>";
+}
+
+$sql = "DELETE FROM `phptrip` WHERE `Dest` = 'Russia' LIMIT 1";
+$result = mysqli_query($conn, $sql);
+$aff = mysqli_affected_rows($conn);
+echo "<br> Number of affected rows: $aff <br>";
+
+if ($result) {
+    echo "Delete successfully";
+} else {
+    $err = mysqli_error($conn);
+    echo "Not Delete successfully due to this error ---> $err";
+}
+?>
